394e686fca820b4ac901a628c9ed9ccaf2ed401f
prakharsam <prakharsam97@gmail.com>
2019-09-27 12:47:50 -0700

    Changes for Student model

--- src/main/java/com/asu/project7/model/Student.java
+++ src/main/java/com/asu/project7/model/Student.java
@@ -1,7 +1,14 @@
 package com.asu.project7.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name="student")
 public class Student {
+
+    @Id
+    @SequenceGenerator(name = "Student_id",sequenceName = "Student_Seq")
+    @GeneratedValue(generator = "Student_id")
+    @Column(name = "studentId")
+    private Long studentId;
 }
